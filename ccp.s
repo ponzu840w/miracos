90b52770ae467fa628e08049dce64387e4eee0fb
mulwak <hideponn2001@gmail.com>
2022-04-21 01:55:55 +0900

    上位に配置

--- ccp.s
+++ ccp.s
@@ -1,4 +1,8 @@
 ; 中国共産党
 ; COSアプリケーションはCCPを食いつぶすことがあり、ウォームブートでカードからリロードされる
 ; つまり特権的地位を持つかもしれないCOSアプリケーションである
+.ZEROPAGE
+.SEGMENT "COMCODE"
+  BRK
+.SEGMENT "COMBSS"
 
--- makeos.sh
+++ makeos.sh
@@ -1,6 +1,8 @@
 # MIRACOS生成専用スクリプト
-cl65 -l ../listing/miracos/bcos.list -m ../listing/miracos/bcos.map -vm -t none -C ./confcos.cfg -o ./bin/bcos.bin ./bcos.s
-objcopy -I binary -O srec --adjust-vma=0x0600 ./bin/bcos.bin ./bin/bcos.srec  # バイアスについては要検討
+cl65 -l ../listing/miracos/bcos.list -m ../listing/miracos/bcos.map -vm -t none -C ./confcos.cfg -o ./bin/BCOS.SYS ./bcos.s
+cl65 -l ../listing/miracos/ccp.list -m ../listing/miracos/ccp.map -vm -t none -C ./confcom.cfg -o ./bin/MIRACOS/CCP.COM ./ccp.s
+objcopy -I binary -O srec --adjust-vma=0x6000 ./bin/BCOS.SYS ./bin/bcos.srec  # バイアスについては要検討
+objcopy -I binary -O srec --adjust-vma=0x0700 ./bin/MIRACOS/CCP.COM ./bin/ccp.srec  # バイアスについては要検討
 
 cat ./bin/bcos.srec | clip.exe
 
