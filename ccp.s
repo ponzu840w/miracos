b2b152be4f08a61b4a99312a2ef45fd99599912c
mulwak <ponzu840w@yahoo.co.jp>
2022-04-23 00:20:28 +0900

    COLORコマンド試験

--- ccp.s
+++ ccp.s
@@ -52,61 +52,112 @@ LOOP:
 ; コマンドライン解析
   LDX #0                          ; 内部コマンド番号初期化
   loadmem16 ZR0,COMMAND_BUF       ; 入力されたコマンドをZR0に
-  loadmem16 ZR1,ICOMMANDS         ; 内部コマンド名称配列をZR1に
+  loadmem16 ZR1,ICOMNAMES         ; 内部コマンド名称配列をZR1に
 @NEXT_ICOM:
   JSR M_EQ                        ; 両ポインタは等しいか？
   BEQ EXEC_ICOM                   ; 等しければ実行する（Xが渡る
+  JSR M_LEN_ZR1
+  CPY #0
+  BEQ ICOM_NOTFOUND
   INY                             ; 内部コマンド名称インデックスを次の先頭に
+  TYA
   CLC
   ADC ZR1                         ; 内部コマンド名称インデックスをポインタに反映
-  TYA
+  STA ZR1
+  LDA #0
   ADC ZR1+1
+  STA ZR1+1
   INX                             ; 内部コマンド番号インデックスを増加
   BNE @NEXT_ICOM                  ; Xが一周しないうちは周回
   ; TODO:ここに内部コマンドが見つからないときのコード
+  JMP ICOM_NOTFOUND
 EXEC_ICOM:                        ; Xで渡された内部コマンド番号を実行する
   TXA
-  JSR BYT2ASC
-  JSR PRT_HEX                     ; とりあえず内部コマンド番号を出力してみる
+  ;JSR PRT_BYT                     ; とりあえず内部コマンド番号を出力してみる
+  ASL
+  TAX
+  JMP (ICOMVECS,X)
   JSR PRT_LF
   BRA LOOP
 
-PRT_HEX:
+; -------------------------------------------------------------------
+;                          内部コマンド
+; -------------------------------------------------------------------
+
+; -------------------------------------------------------------------
+;                        ROMモニタに落ちる
+; -------------------------------------------------------------------
+ICOM_EXIT:
+  loadAY16 STR_GOODBYE
+  syscall CON_OUT_STR
+  BRK
+  NOP
+  JMP LOOP
+
+; -------------------------------------------------------------------
+;                        画面の色を変える
+; -------------------------------------------------------------------
+ICOM_COLOR:
+  loadAY16 STR_ICOM_COLOR_START
+  syscall CON_OUT_STR                 ; 説明文
+  LDA #1
+  syscall GCHR_COL
+  TAY
+  INY
+  LDA #3
+  syscall GCHR_COL
+  JMP LOOP
+
+; -------------------------------------------------------------------
+;                          見つからない
+; -------------------------------------------------------------------
+ICOM_NOTFOUND:
+ICOM_CD:
+ICOM_REBOOT:
+  loadAY16 STR_COMNOTFOUND
+  syscall CON_OUT_STR
+  JMP LOOP
+
+; -------------------------------------------------------------------
+;                          汎用関数群
+; -------------------------------------------------------------------
+; どうする？ライブラリ？システムコール？
+; -------------------------------------------------------------------
+PRT_BYT:
   JSR BYT2ASC
   PHY
-  JSR @call
+  JSR @CALL
   PLA
-@call:
+@CALL:
   syscall CON_OUT_CHR
   RTS
 
 BYT2ASC:
   ; Aで与えられたバイト値をASCII値AYにする
   ; Aから先に表示すると良い
-  PHA
-  LSR
-  LSR
-  LSR
-  LSR
-  JSR PRHEXZ
-  STA ZR0
-  PLA
+  PHA           ; 下位のために保存
   AND #$0F
-  JSR PRHEXZ
-  LDY ZR0
+  JSR NIB2ASC
+  TAY
+  PLA
+  LSR           ; 右シフトx4で上位を下位に持ってくる
+  LSR
+  LSR
+  LSR
+  JSR NIB2ASC
   RTS
 
-PRHEXZ:
+NIB2ASC:
+  ; #$0?をアスキー一文字にする
   ORA #$30
   CMP #$3A
-  BCC PRT_BYT1
+  BCC @SKP_ADC  ; Aが$3Aより小さいか等しければ分岐
   ADC #$06
-PRT_BYT1:
+@SKP_ADC:
   RTS
 
 M_EQ_AY:
   ; AYとZR0が等しいかを返す
-  ; YはAYの終端文字を指すインデックス
   STA ZR1
   STY ZR1+1
 M_EQ:
@@ -127,6 +178,20 @@ M_EQ:
   CLC
   RTS
 
+M_LEN:
+  ; 文字列の長さを取得する
+  ; input:AY
+  ; output:Y
+  STA ZR1
+  STY ZR1+1
+M_LEN_ZR1:  ; ZR1入力
+  LDY #$FF
+@LOOP:
+  INY
+  LDA (ZR1),Y
+  BNE @LOOP
+  RTS
+
 PRT_LF:
   ; 改行
   LDA #$A
@@ -138,12 +203,22 @@ PRT_LF:
 ;                             データ領域
 ; -------------------------------------------------------------------
 STR_INITMESSAGE:  .BYT "MIRACOS 0.01 for FxT-65",$A,$A,$0 ; 起動時メッセージ
+STR_COMNOTFOUND:  .BYT "Unknown Command.",$A,$0
+STR_ICOM_COLOR_START:  .BYT "Console Color Setting.",$A,"J,K: Character",$A,"H,L: Background",$A,"ENTER: Complete",$0
+STR_GOODBYE:  .BYT "Good Bye.",$A,$0
 PATH_DEFAULT:     .BYT "A:/"
 
 ; -------------------------------------------------------------------
 ;                        内部コマンドテーブル
 ; -------------------------------------------------------------------
-ICOMMANDS:        .ASCIIZ "EXIT"        ; 0
+ICOMNAMES:        .ASCIIZ "EXIT"        ; 0
                   .ASCIIZ "CD"          ; 1
                   .ASCIIZ "REBOOT"      ; 2
+                  .ASCIIZ "COLOR"       ; 3
+                  .BYT $0
+
+ICOMVECS:         .WORD ICOM_EXIT
+                  .WORD ICOM_CD
+                  .WORD ICOM_REBOOT
+                  .WORD ICOM_COLOR
 
