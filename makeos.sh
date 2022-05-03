f90af41a886a19b52799cd04a7649c8ce654f50d
mulwak <ponzu840w@yahoo.co.jp>
2022-05-03 14:06:39 +0900

    makecos.shを更新してバイナリとリスティングの出力先ディレクトリを作るように

--- makeos.sh
+++ makeos.sh
@@ -1,5 +1,7 @@
 # MIRACOS生成専用スクリプト
-cl65 -l ../listing/miracos/bcos.list -m ../listing/miracos/bcos.map -vm -t none -C ./confcos.cfg -o ./bin/BCOS.SYS ./bcos.s
+mkdir ./listing -p
+mkdir ./bin/MIRACOS -p
+cl65 -l ./listing/bcos.list -m ./listing/bcos.map -vm -t none -C ./confcos.cfg -o ./bin/BCOS.SYS ./bcos.s
 objcopy -I binary -O srec --adjust-vma=0x6000 ./bin/BCOS.SYS ./bin/bcos.srec  # バイアスについては要検討
 objcopy -I binary -O srec --adjust-vma=0x5000 ./bin/MIRACOS/CCP.SYS ./bin/ccp.srec  # バイアスについては要検討
 objcopy -I binary -O srec --adjust-vma=0x0600 ./bin/MIRACOS/SYSCALL.SYS ./bin/syscall.srec  # バイアスについては要検討
