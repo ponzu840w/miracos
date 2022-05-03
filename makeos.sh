055b45c6e17bd7622ec31ed3b55bdebce8347ab4
mulwak <ponzu840w@yahoo.co.jp>
2022-05-03 14:31:24 +0900

    シンボルリストも出力するように

--- makeos.sh
+++ makeos.sh
@@ -1,7 +1,7 @@
 # MIRACOS生成専用スクリプト
 mkdir ./listing -p
 mkdir ./bin/MIRACOS -p
-cl65 -l ./listing/bcos.list -m ./listing/bcos.map -vm -t none -C ./confcos.cfg -o ./bin/BCOS.SYS ./bcos.s
+cl65 -g -Wl -Ln,./listing/symbol-bcos.s  -l ./listing/list-bcos.s -m ./listing/map-bcos.s -vm -t none -C ./confcos.cfg -o ./bin/BCOS.SYS ./bcos.s
 objcopy -I binary -O srec --adjust-vma=0x6000 ./bin/BCOS.SYS ./bin/bcos.srec  # バイアスについては要検討
 objcopy -I binary -O srec --adjust-vma=0x5000 ./bin/MIRACOS/CCP.SYS ./bin/ccp.srec  # バイアスについては要検討
 objcopy -I binary -O srec --adjust-vma=0x0600 ./bin/MIRACOS/SYSCALL.SYS ./bin/syscall.srec  # バイアスについては要検討
