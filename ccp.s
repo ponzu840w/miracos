f62b392d6611a4e69b226ea1bc0bc711fa041672
mulwak <ponzu840w@yahoo.co.jp>
2022-05-12 18:53:33 +0900

    dirコマンドでハイフンと文字による属性表示

--- ccp.s
+++ ccp.s
@@ -28,6 +28,7 @@ ZR3 = ZR2+2
 ZR4 = ZR3+2
 ZR5 = ZR4+2
 .ZEROPAGE
+ZP_ATTR:          .RES 1  ; 属性バイトをシフトして遊ぶ
 
 .BSS
 COMMAND_BUF:      .RES 64 ; コマンド入力バッファ
@@ -170,27 +171,47 @@ ICOM_DIR:
 @FIND:
   mem2AY16 ZR0
   syscall FS_FIND_FST
-  storeAY16 ZR4
+  storeAY16 ZR4                 ; FINFOをZR4に退避
   BCS @ERR
-  INC
-  syscall CON_OUT_STR
-  JSR PRT_LF
-@LOOP:
-  mem2AY16 ZR4
-  syscall FS_FIND_NXT
-  BCS @END
-  INC
-  syscall CON_OUT_STR           ; ファイル名を出力
-  mem2AY16 ZR4
-  storeAY16 ZR0
+  BRA @FSTENT
+@LOOP:                          ; 発見したエントリごとのループ
+  mem2AY16 ZR4                  ; 前回のFINFO
+  syscall FS_FIND_NXT           ; 次のエントリを検索
+  BCS @END                      ; もう見つからなければ終了
+@FSTENT:                        ; 初回に見つかったらここに飛ぶ
+  ; 属性
+  storeAY16 ZR0                 ; オフセットを加えたりして参照するためにFINFOをZR0に
   LDA ZR0                       ; 下位桁取得
   ADC #FINFO::ATTR              ; 属性を取得したいのでオフセット加算
   STA ZR0
   BCC @SKP_C
   INC ZR0+1
 @SKP_C:
-  LDA (ZR0)
-  JSR PRT_BYT
+  LDA (ZR0)                     ; 属性バイト取得
+  STA ZP_ATTR                   ; 専用ZPに格納
+  LDY #$0
+  ASL ZP_ATTR                   ; 上位2bitを捨てる
+  ASL ZP_ATTR
+@ATTRLOOP:
+  ASL ZP_ATTR                   ; C=ビット情報
+  BCS @ATTR_CHR
+  LDA #'-'                      ; そのビットが立っていないときはハイフンを表示
+  BRA @SKP_ATTR_CHR
+@ATTR_CHR:
+  LDA STR_ATTR,Y                ; 属性文字を表示
+@SKP_ATTR_CHR:
+  PHY
+  syscall CON_OUT_CHR           ; 属性文字/-を表示
+  PLY
+  INY
+  CPY #6
+  BNE @ATTRLOOP
+  JSR PRT_S                     ; 区切りスペース
+  ; ファイル名
+  mem2AY16 ZR4
+  INC
+  syscall CON_OUT_STR           ; ファイル名を出力
+  ; 改行
   JSR PRT_LF
   BRA @LOOP
 @END:
@@ -198,6 +219,8 @@ ICOM_DIR:
 @ERR:
   JSR BCOS_ERROR
   BRA @END
+
+STR_ATTR: .BYT  "advshr"
 
 ; -------------------------------------------------------------------
 ;                     カレントディレクトリ変更
@@ -304,11 +327,21 @@ PRT_BIN:
 PRT_BYT:
   JSR BYT2ASC
   PHY
-  JSR @CALL
+  JSR PRT_C_CALL
   PLA
-@CALL:
+PRT_C_CALL:
   syscall CON_OUT_CHR
   RTS
+
+PRT_LF:
+  ; 改行
+  LDA #$A
+  JMP PRT_C_CALL
+
+PRT_S:
+  ; スペース
+  LDA #' '
+  JMP PRT_C_CALL
 
 BYT2ASC:
   ; Aで与えられたバイト値をASCII値AYにする
@@ -382,12 +415,6 @@ M_CP_AYS:
   STA (ZR1),Y
   BEQ M_LEN_RTS
   BRA @LOOP
-
-PRT_LF:
-  ; 改行
-  LDA #$A
-  syscall CON_OUT_CHR
-  RTS
 
 ANALYZE_PATH:
   ; あらゆる種類のパスを解析する
