09b729aeb17aee0f36f024966fa59b10779885e4
mulwak <ponzu840w@yahoo.co.jp>
2022-05-14 01:10:15 +0900

    hello正常化

--- com/hello.s
+++ com/hello.s
@@ -21,6 +21,6 @@ START:
   syscall CON_OUT_STR
   RTS                             ; BCOS0でも等価でありたいが現状そうでないのでリターン
 
-;STR_HELLO: .ASCIIZ "hello, world"
-STR_HELLO: .ASCIIZ "hell!!!!"
+STR_HELLO: .ASCIIZ "hello, world",$A
+;STR_HELLO: .ASCIIZ "hell!!!!"
 
