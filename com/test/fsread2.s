95bcc5bf8a49bf7fe45704b45385089d23c8f97f
mulwak <hideponn2001@gmail.com>
2022-06-03 21:52:59 +0900

    テストプログラムのパス指定とlength指定

--- com/test/fsread2.s
+++ com/test/fsread2.s
@@ -17,7 +17,6 @@
 ; -------------------------------------------------------------------
 ;                             定数定義
 ; -------------------------------------------------------------------
-LENGTH  = $23
 BFPTR   = BUFFER
 
 ; -------------------------------------------------------------------
@@ -29,17 +28,29 @@ BFPTR   = BUFFER
   FCTRL_SAV:      .RES 2
   TMP32:          .RES 4
   ACTLEN:         .RES 2
+  REQLEN:         .RES 2
 
 ; -------------------------------------------------------------------
 ;                             実行領域
 ; -------------------------------------------------------------------
 .CODE
 START:
+  pushAY16
   ; 挨拶
   loadAY16 STR_HELLO
   syscall CON_OUT_STR
-  ; ファイル検索
+  pullAY16
+  ; nullチェック
+  storeAY16 ZR0
+  TAX
+  LDA (ZR0)
+  BEQ @TEST_TXT ; コマンドライン引数がないならデフォルトのパス
+  TXA
+  BRA @ARG
+@TEST_TXT:
   loadAY16 PATH_DATA
+@ARG:
+  ; ファイル検索
   syscall FS_FIND_FST             ; 検索
   ;BCS NOTFOUND                    ; 見つからなかったらあきらめる
   BCC @SKP_NOTFOUND
@@ -59,11 +70,27 @@ START:
   LDA FD_SAV
   JSR PRT_BYT
   JSR PRT_LF
+  ; 要求LENGTHの入力
+  ; LENGTH?プロンプト
+@INPUT_LENGTH:
+  loadAY16 STR_LENGTH
+  syscall CON_OUT_STR
+  ; 文字列入力
+  LDA #8                          ; 入力バッファ長さは8
+  STA ZR0
+  loadAY16 INSTR_BF               ; 入力バッファのポインタ
+  syscall CON_IN_STR
+  ; 文字列の変換
+  loadAY16 INSTR_BF               ; 入力バッファのポインタ
+  JSR STR2NUM
+  BCS @INPUT_LENGTH               ; 失敗したらリトライ
+  mem2mem16 REQLEN,ZR1
+  JSR PRT_LF
   ; コール
   LDA FD_SAV
   STA ZR1
   loadmem16 ZR0,BFPTR
-  loadAY16 LENGTH
+  mem2AY16 REQLEN
   syscall FS_READ_BYTS2           ; コール
   storeAY16 FCTRL_SAV             ; FCTRLを取得
   mem2mem16 ACTLEN,ZR2            ; 16bit値を保存
@@ -98,19 +125,97 @@ START:
   ; ACTLEN
   loadAY16 ACTLEN
   JSR PRT_SHORT_LF
+  ; ファイルクローズ
+  LDA FD_SAV
+  syscall FS_CLOSE
   RTS
 
+; ファイルが見つからないとか開けないとか
 NOTFOUND:
   loadAY16 STR_NOTFOUND
   syscall CON_OUT_STR
   RTS
 
+; カーネルエラーの表示
 BCOS_ERROR:
   JSR PRT_LF
   syscall ERR_GET
   syscall ERR_MES
   RTS
 
+; ASCII文字列をHEXと信じて変換
+STR2NUM:
+  @STR_PTR=ZR0
+  @NUMBER16=ZR1
+  storeAY16 @STR_PTR
+  STZ ZR1
+  STZ ZR1+1
+  ; 最後尾まで探索、余計な文字があったらエラー
+  LDY #$FF
+@FIND_EOS_LOOP:
+  INY
+  LDA (@STR_PTR),Y
+  BNE @FIND_EOS_LOOP
+@END_OF_STR:
+  ; Y=\0
+  LDX #0
+@BYT_LOOP:
+  ; 下位nibble
+  DEY
+  CPY #$FF
+  BEQ @END
+  LDA (@STR_PTR),Y
+  JSR CHR2NIB
+  BCS @ERR
+  STA ZR1,X
+  ; 上位nibble
+  DEY
+  CPY #$FF
+  BEQ @END
+  LDA (@STR_PTR),Y
+  JSR CHR2NIB
+  BCS @ERR
+  ASL
+  ASL
+  ASL
+  ASL
+  ORA ZR1,X
+  STA ZR1,X
+  INX
+  BRA @BYT_LOOP
+@END:
+  CLC
+  RTS
+@ERR:
+  SEC
+  RTS
+
+; *
+; --- Aレジスタの一文字をNibbleとして値にする ---
+; *
+CHR2NIB:
+  CMP #'0'
+  BMI @ERR
+  CMP #'9'+1
+  BPL @ABCDEF
+  SEC
+  SBC #'0'
+  CLC
+  RTS
+@ABCDEF:
+  CMP #'A'
+  BMI @ERR
+  CMP #'F'+1
+  BPL @ERR
+  SEC
+  SBC #'A'-$0A
+  CLC
+  RTS
+@ERR:
+  SEC
+  RTS
+
+; 16bit値を表示+改行
 PRT_SHORT_LF:
   storeAY16 ZR2
   LDY #1
@@ -121,10 +226,12 @@ PRT_SHORT_LF:
   JSR PRT_BYT
   JMP PRT_LF
 
+; 32bit値を表示+改行
 PRT_LONG_LF:
   JSR PRT_LONG
   JMP PRT_LF
 
+; 32bit値を表示
 PRT_LONG:
   storeAY16 ZR2
   LDY #3
@@ -137,6 +244,7 @@ PRT_LONG:
   BPL @LOOP
   RTS
 
+; 8bit値を表示
 PRT_BYT:
   JSR BYT2ASC
   PHY
@@ -146,19 +254,19 @@ PRT_C_CALL:
   syscall CON_OUT_CHR
   RTS
 
+; 改行
 PRT_LF:
-  ; 改行
   LDA #$A
   JMP PRT_C_CALL
 
+; スペース印字
 PRT_S:
-  ; スペース
   LDA #' '
   JMP PRT_C_CALL
 
+; Aで与えられたバイト値をASCII値AYにする
+; Aから先に表示すると良い
 BYT2ASC:
-  ; Aで与えられたバイト値をASCII値AYにする
-  ; Aから先に表示すると良い
   PHA           ; 下位のために保存
   AND #$0F
   JSR NIB2ASC
@@ -171,8 +279,8 @@ BYT2ASC:
   JSR NIB2ASC
   RTS
 
+; #$0?をアスキー一文字にする
 NIB2ASC:
-  ; #$0?をアスキー一文字にする
   ORA #$30
   CMP #$3A
   BCC @SKP_ADC  ; Aが$3Aより小さいか等しければ分岐
@@ -191,11 +299,13 @@ PATH_DATA:
   .ASCIIZ "A:/TEST.TXT"
 
 STR_HELLO:      .BYT "New FS_READ syscall dev tool",$A,$0
-STR_GOT_FD:     .BYT "File Descriptor: $",$0
+STR_LENGTH:     .BYT "Length?   :     $",$0
+STR_GOT_FD:     .BYT "File Dscr.:       $",$0
 STR_FCTRL_SIZ:  .BYT "FCTRL_SIZE: $",$0
 STR_FCTRL_SEEK: .BYT "FCTRL_SEEK: $",$0
 STR_TMP32:      .BYT "TMP32     : $",$0
-STR_ACTLEN:     .BYT "ACTLEN    : $",$0
+STR_ACTLEN:     .BYT "ACTLEN    :     $",$0
 
+INSTR_BF: .RES 8
 BUFFER:
 
