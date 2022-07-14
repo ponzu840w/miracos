316e544afa0d1b07452eb6a1df4063a7378b1869
mulwak <ponzu840w@yahoo.co.jp>
2022-07-14 15:54:37 +0900

    hello2の何らかの変更(覚えていない、たぶん駄目だったんだろう

--- cc/hello2.c
+++ cc/hello2.c
@@ -2,15 +2,20 @@
  * sprintf
  */
 #include <stdio.h>
-extern void __fastcall__ couts(char *str);
+#include <string.h>
+extern void couts(char *str);
 
 int main(){
   int i;
-  char buf[32];
+  char str1[32]="ABC";
+  //char str2[]="123";
+  char* cptr;
   for(i=0;i<5;i++){
     //couts("hello,world\n");
-    sprintf(buf, "hello,world [%d] hello,stdio", i);
-    couts(buf);
+    //strcat(str1,str2);                      // 機能する
+    //sprintf(cptr, "hello,stdio [%d]\n", i);
+    couts(cptr);
+    couts(str1);
   }
   return (0);
 }
