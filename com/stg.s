00e487ddc04848fd85968b8bdca69d99e691f9e6
mulwak <hideponn2001@gmail.com>
2022-08-14 00:53:40 +0900

    se分離

--- com/stg.s
+++ com/stg.s
@@ -50,7 +50,7 @@ TOP_MARGIN = 8*3
   ZP_PL_COOLDOWN:     .RES 1
   ZP_BL_INDEX:        .RES 1        ; ブラックリストのYインデックス退避
   ZP_PLBLT_TERMIDX:   .RES 1        ; PLBLT_LSTの終端を指す
-  ZP_ENEM1_TERMIDX:   .RES 1        ; ENEM1_LSTの終端を指す
+  ZP_ENEM1_TERMIDX:    .RES 1        ; ENEM1_LSTの終端を指す
   ZP_DMK1_TERMIDX:    .RES 1        ; DMK1_LSTの終端を指す
 
 ; -------------------------------------------------------------------
@@ -58,6 +58,7 @@ TOP_MARGIN = 8*3
 ; -------------------------------------------------------------------
 .SEGMENT "LIB"
 .INCLUDE "./+stg/dmk.s"
+.INCLUDE "./+stg/se.s"
 
 ; -------------------------------------------------------------------
 ;                              変数領域
@@ -73,8 +74,8 @@ TOP_MARGIN = 8*3
   BLACKLIST2:     .RES 256
   ; プレイヤの発射した弾丸
   PLBLT_LST:     .RES 32  ; (X,Y),(X,Y),...
-  ; 敵1
-  ENEM1_LST:     .RES 32  ; (X,Y,T),(X,Y,T),...
+  ; 敵
+  ENEM1_LST:      .RES 32  ; (code,X,Y,f),(code,X,Y,f),...
   ; 弾幕1
   DMK1_LST:      .RES 256 ; (X,Y,dX,dY),...
 
@@ -82,7 +83,6 @@ TOP_MARGIN = 8*3
 ;                             実行領域
 ; -------------------------------------------------------------------
 .CODE
-  ;JMP INIT
 
 ; -------------------------------------------------------------------
 ;                         プログラム初期化
@@ -412,6 +412,19 @@ TICK_ENEM1:
   ;   PLBLTループを抜け、描画処理
   PLY                       ; BLIDX復帰
   LDA ENEM1_LST,X
+  ;ADC #$80
+  CLC
+  ADC #256-1
+  ;BVC @SKP_DEL_LEFT
+  ; ENEM1削除
+  ;PHY
+  ;JSR DEL_ENEM1
+  ;PLY
+  ;JMP @DRAWPLBL
+@SKP_DEL_LEFT:
+  ;SEC
+  ;SBC #$80
+  STA ENEM1_LST,X
   STA ZP_CANVAS_X           ; 描画用座標
   STA (ZP_BLACKLIST_PTR),Y  ; BL格納
   INX                       ; Y座標へ
@@ -486,91 +499,6 @@ TICK_PL_BLT:
   INY
   STA (ZP_BLACKLIST_PTR),Y
 .endmac
-
-; -------------------------------------------------------------------
-;                   YMZ内部レジスタに値を格納する
-; -------------------------------------------------------------------
-.macro set_ymzreg addr,dat
-  LDA addr
-  STA YMZ::ADDR
-  LDA dat
-  STA YMZ::DATA
-.endmac
-
-; -------------------------------------------------------------------
-;                   Aで与えられた番号のSEを鳴らす
-; -------------------------------------------------------------------
-; X使用
-PLAY_SE:
-  STA ZP_SE_STATE
-  LSR
-  TAX
-  LDA SE_LENGTH_TABLE-1,X
-  STA ZP_SE_TIMER
-@END:
-  RTS
-
-; -------------------------------------------------------------------
-;                       効果音ティック処理
-; -------------------------------------------------------------------
-.macro tick_se
-TICK_SE:
-  LDX ZP_SE_STATE       ; 効果音状態
-  BEQ TICK_SE_END       ; 何も鳴ってないなら無視
-  JMP (SE_TICK_JT-2,X)  ; 鳴っているので効果音種類ごとの処理に跳ぶ
-TICK_SE_RETURN:         ; ここに帰ってくる
-  DEC ZP_SE_TIMER       ; タイマー減算
-  BNE TICK_SE_END
-  ; 0になった
-  set_ymzreg #YMZ::IA_MIX,#%00111111
-  STZ ZP_SE_STATE
-TICK_SE_END:
-.endmac
-
-; -------------------------------------------------------------------
-;                        効果音種類テーブル
-; -------------------------------------------------------------------
-SE_LENGTH_TABLE:
-  .BYTE SE1_LENGTH      ; 1
-  .BYTE SE2_LENGTH      ; 2
-
-SE_TICK_JT:
-  .WORD SE1_TICK
-  .WORD SE2_TICK
-
-; -------------------------------------------------------------------
-;                         各効果音ティック
-; -------------------------------------------------------------------
-SE1_TICK:
-  LDA ZP_SE_TIMER
-  CMP #SE1_LENGTH
-  BNE @a
-  set_ymzreg #YMZ::IA_MIX,#%00111110
-  set_ymzreg #YMZ::IA_FRQ+1,#>(125000/800)
-  set_ymzreg #YMZ::IA_FRQ,#<(125000/800)
-  set_ymzreg #YMZ::IA_VOL,#$0F
-  JMP TICK_SE_RETURN
-@a:
-  LDX #YMZ::IA_VOL
-  STX YMZ::ADDR
-  ASL                       ; タイマーの左シフト、最大8
-  ADC #4
-  STA YMZ::DATA
-  JMP TICK_SE_RETURN
-
-SE2_TICK:
-  set_ymzreg #YMZ::IA_MIX,#%00110111
-  set_ymzreg #YMZ::IA_NOISE_FRQ,#>(125000/400)
-  set_ymzreg #YMZ::IA_VOL,#$0F
-  JMP TICK_SE_RETURN
-
-; -------------------------------------------------------------------
-;                           効果音定数
-; -------------------------------------------------------------------
-SE1_LENGTH = 5
-SE1_NUMBER = 1*2
-SE2_LENGTH = 5
-SE2_NUMBER = 2*2
 
 ; -------------------------------------------------------------------
 ;                        垂直同期割り込み
