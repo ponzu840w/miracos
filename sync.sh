382089f71f9680cf0e198981ce0c52a9493feddc
mulwak <ponzu840w@yahoo.co.jp>
2023-02-28 00:28:49 +0900

    umount

--- sync.sh
+++ sync.sh
@@ -6,4 +6,5 @@ echo "=== SDカード -> FT65SYNC ==="
 rsync -rthvuc /mnt/sd/ /home/ponzu840w/wh/OneDrive/FxT-65/FT65SYNC/
 echo "=== FT65SYNC -> SDカード ==="
 rsync -rthvuc /home/ponzu840w/wh/OneDrive/FxT-65/FT65SYNC/ /mnt/sd/
+umount /mnt/sd
 
