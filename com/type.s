c8fb1aad4c59ae6281410906362017cad4bf2210
mulwak <ponzu840w@yahoo.co.jp>
2023-06-18 03:07:28 +0900

    typeのバッファを512バイトに

--- com/type.s
+++ com/type.s
@@ -12,12 +12,15 @@
 .INCLUDE "../fs/structfs.s"   ; ファイルシステム関連構造体定義
 .INCLUDE "../zr.inc"          ; ZPレジスタZR0..ZR5
 
+FILE_BUF_SIZE = 512
+
 ; -------------------------------------------------------------------
 ;                             ZP変数領域
 ; -------------------------------------------------------------------
 .ZEROPAGE
-  FD_SAV:         .RES 1  ; ファイル記述子
-  FINFO_SAV:      .RES 2  ; FINFO
+  FD_SAV:         .RES 1    ; ファイル記述子
+  FINFO_SAV:      .RES 2    ; FINFO
+  FILE_BUF_PTR:   .RES 2    ; ファイルバッファ上のどこかを指すポインタ
 
 ; -------------------------------------------------------------------
 ;                             実行領域
@@ -28,7 +31,7 @@ START:
   ;   初期化
   ; ---------------------------------------------------------------
   ;   バッファ終端設定
-  STZ BUFFER+256
+  STZ FILE_BUF+FILE_BUF_SIZE
   ; ---------------------------------------------------------------
   ;   コマンドライン引数処理
   storeAY16 ZR0                   ; ZR0=arg
@@ -54,23 +57,25 @@ LOOP:
   ;   バッファへのデータロード
   LDA FD_SAV
   STA ZR1                         ; ZR1 = FD
-  loadmem16 ZR0,BUFFER            ; ZR0 = 書き込み先
-  loadAY16 256                    ; AY  = 読み取り長さ
+  loadmem16 ZR0,FILE_BUF          ; ZR0 = 書き込み先
+  loadAY16 FILE_BUF_SIZE          ; AY  = 読み取り長さ
   syscall FS_READ_BYTS            ; 以上設定で読み取り
   ; ---------------------------------------------------------------
   ;   ロード結果への対応
   BCS @CLOSE                      ; 初手EOF -> 終了
-  TAX                             ; 読み取ったバイト数
-  CPY #1                          ; 256バイト読んだか？
-  BEQ @SKP_EOF
-  STZ BUFFER,X                    ; バッファ中にEOF -> 終端設置
-@SKP_EOF:
+  CLC                             ; *
+  ADC #<FILE_BUF                  ; | *FILE_BUF_PTR = &FILE_BUF + len;
+  STA FILE_BUF_PTR                ; |
+  TYA                             ; |
+  ADC #>FILE_BUF                  ; |
+  STA FILE_BUF_PTR+1              ; |
+  LDA #0
+  STA (FILE_BUF_PTR)              ; 終端
   ; ---------------------------------------------------------------
   ;   バッファ内容の出力
-  loadAY16 BUFFER
+  loadAY16 FILE_BUF
   syscall CON_OUT_STR
   BRA LOOP
-  ; 最終バイトがあるとき
 
   ; ---------------------------------------------------------------
   ;   終了処理
@@ -94,7 +99,7 @@ BCOS_ERROR:
   syscall CON_OUT_CHR
   syscall ERR_GET
   syscall ERR_MES
-  JMP LOOP
+  RTS
 
 STR_NOTFOUND:
   .BYT "Input File Not Found.",$A,$0
@@ -103,5 +108,5 @@ STR_NOTFOUND:
 ;                            バッファ領域
 ; -------------------------------------------------------------------
 .BSS
-BUFFER:
+FILE_BUF:
 
