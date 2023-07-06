d313fb4598457c03d53e7f023fd5b587ad88ce48
mulwak <ponzu840w@yahoo.co.jp>
2023-07-07 00:02:48 +0900

    cc_sprintfテスト成功

--- cc/hello2.c
+++ cc/hello2.c
@@ -8,12 +8,12 @@ extern void couts(char *str);
 int main(){
   int i;
   char str1[32]="ABC";
-  //char str2[]="123";
+  char str2[]="123";
   char* cptr;
   for(i=0;i<5;i++){
-    //couts("hello,world\n");
-    //strcat(str1,str2);                      // 機能する
-    //sprintf(cptr, "hello,stdio [%d]\n", i);
+    couts("hello,world\n");
+    strcat(str1,str2);                      // 機能する
+    sprintf(cptr, "hello,stdio [%d]\n", i);
     couts(cptr);
     couts(str1);
   }
