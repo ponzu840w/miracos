1bf5eebf013265584ca2160d5cd41b2580dfc937
mulwak <ponzu840w@yahoo.co.jp>
2024-02-01 22:22:59 +0900

    [fix]sync.shのドライブレターをコマンドライン引数にした

--- sync.sh
+++ sync.sh
@@ -1,10 +1,14 @@
 set -eu
+
+drive_letter=$1
+echo "drive:"${drive_letter}
+
 echo "===    bin   -> FT65SYNC ==="
-rsync -rchvu bin/* /home/ponzu840w/wh/OneDrive/FxT-65/FT65SYNC/
-mount -t drvfs f: /mnt/sd/
+rsync -rchvu --omit-dir-times bin/* /home/ponzu840w/wh/OneDrive/FxT-65/FT65SYNC/
+mount -t drvfs ${drive_letter}: /mnt/sd/
 #echo "=== SDカード -> FT65SYNC ==="
 #rsync -rthvuc /mnt/sd/ /home/ponzu840w/wh/OneDrive/FxT-65/FT65SYNC/
 echo "=== FT65SYNC -> SDカード ==="
-rsync -rthvuc /home/ponzu840w/wh/OneDrive/FxT-65/FT65SYNC/ /mnt/sd/
+rsync -rthvuc --omit-dir-times /home/ponzu840w/wh/OneDrive/FxT-65/FT65SYNC/ /mnt/sd/
 umount /mnt/sd
 
