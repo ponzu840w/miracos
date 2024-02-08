106807d7e90f8f9332795a4f01943a2afd8bbb69
mulwak <ponzu840w@yahoo.co.jp>
2024-02-08 16:08:02 +0900

    [add]zd_iとaを追加

--- com/zd.c
+++ com/zd.c
@@ -32,7 +32,8 @@ void put_line(char* str){
   coutc('\n');
 }
 
-// 行番号から行のポインタを得る
+/* 行番号から行のポインタを得る */
+/* 最終行+1を指定するとEOTの場所が返る */
 char* getLine(unsigned int line_num){
   unsigned int cnt = 1;
   char* ptr = text_buffer;
@@ -51,7 +52,7 @@ char* getLine(unsigned int line_num){
     }
   }while(*(ptr++)!='\0');
 
-  return NULL;
+  return --ptr;
 }
 
 // なんか都合のいいatoi
@@ -118,11 +119,12 @@ unsigned int makeGap(char* to, char* from){
 
   /* 移動先が移動元より前にあるエラー */
   if(to <= from)
-    return 0;
+    return 2;
 
   do{
+    //printf("len=%u, from:%d, from_work:%d, char=%c\n",length,from,from_work,*from_work);
     *(to_work--) = *(from_work--);
-  }while(--i == 0);
+  }while(i-- > 0);
   return 1;
 }
 
@@ -130,10 +132,35 @@ unsigned int makeGap(char* to, char* from){
 unsigned int closeGap(){
 }
 
+/* i,a,c共通部分 */
+/* 0 <= line <= lastl+1 */
+void insertLine(unsigned int line){
+  unsigned int len;
+  char* ptr = getLine(line);    /* 操作対象行 */
+  cins(line_buffer);            /* 入力バッファに入力 */
+  len = strlen(line_buffer)+1;
+
+  /* 末尾でなければスキマを作る */
+  if(line <= lastl){
+    makeGap(ptr+len, ptr);
+  }else{
+    *(ptr++) = '\n';  /* 末尾ならEOTを改行にしてポインタを進める */
+  }
+
+  /* スキマに入力をコピー */
+  strcpy(ptr, line_buffer);
+
+  /* 末尾でなければ挿入の終端を改行にする */
+  if(line <= lastl)
+    *(ptr+len-1) = '\n';
+
+  /* 1行増えたので最終行を増加 */
+  lastl++;
+}
+
 int main(void){
-  //char* line_ptr;
   char verb;
-  unsigned int i, len;
+  unsigned int i;
 
   // テスト用テキストでバッファを初期化
   strcpy(text_buffer,"Line1. This is Text Buffer.\nLine2. New Line\nLine3.\nLine4. Good Bye.");
@@ -141,8 +168,8 @@ int main(void){
   printf("sample_text:\n%s\n",text_buffer);
   printf("text_buffer:%d\n",&text_buffer);
 
-  makeGap(getLine(3),getLine(2));
-  printf("gaped_text:\n%s\n",text_buffer);
+  //printf("makeGap:%u\n",makeGap(getLine(3),getLine(2)));
+  //printf("gaped_text:\n%s\n",text_buffer);
 
   cl = 1;
   lastl = 4;
@@ -166,13 +193,15 @@ int main(void){
     verb = command[cmd_verb_index];
     switch(verb){
       case 'p': /* print */
-        for(i=cl_left;i<=cl_right;i++){
+        for(i=cl_left; i <= cl_right; i++){
           put_line(getLine(i));
         }
         break;
+      case 'i': /* insert */
+        insertLine(cl_right);
+        break;
       case 'a': /* append */
-        cins(line_buffer);
-        strlen(line_buffer);
+        insertLine(cl_right+1);
         break;
     }
 
