796d0733ef830f422b2341f11be58a5141dc5f0d
ponzu840w <ponzu840w@yahoo.co.jp>
2026-01-24 16:47:33 +0900

    [paint]トグル式カーソル

--- com/paint.s
+++ com/paint.s
@@ -17,45 +17,35 @@
 CURSOR_SPEED = 1      ; カーソル速度
 TOP_MARGIN = 8*3      ; 上部のマージン
 RL_MARGIN = 4         ; 左右のマージン
+COL_CORSOR_BG = $FF
+COL_CORSOR_FG = $00
 
 ; -------------------------------------------------------------------
 ;                               ZP領域
 ; -------------------------------------------------------------------
 .ZEROPAGE
-  ZP_CURSOR_X:  .RES 1
-  ZP_CURSOR_Y:  .RES 1
-  ZP_CURSOR_DX: .RES 1
-  ZP_CURSOR_DY: .RES 1
-  ZP_PADSTAT:   .RES 2        ; ゲームパッドの状態が収まる
-  ZP_SHIFTER:   .RES 1        ; ゲームパッド読み取り処理用
-  ZP_VB_STUB:   .RES 2        ; 割り込み終了処理
-  HOGE:         .RES 1        ; デバッグ用
+  ZP_CURSOR_X:      .RES 1
+  ZP_CURSOR_Y:      .RES 1
+  ZP_CURSOR_DX:     .RES 1
+  ZP_CURSOR_DY:     .RES 1
+  ZP_PADSTAT:       .RES 2        ; ゲームパッドの状態が収まる
+  ZP_PADSTAT_PREV:  .RES 2        ; ゲームパッドの状態が収まる
+  ZP_SHIFTER:       .RES 1        ; ゲームパッド読み取り処理用
+  ZP_VB_STUB:       .RES 2        ; 割り込み終了処理
+  ZP_DISP_FRAME:    .RES 1        ; 表示中のフレームバッファ
+  HOGE:             .RES 1        ; デバッグ用
 
 ; -------------------------------------------------------------------
 ;                             実行領域
 ; -------------------------------------------------------------------
-.macro init_crtc
-  ; CRTCを初期化
-  LDA #%10000000                  ; ChrBox off
-  STA CRTC2::CHRW
-  ; コンフィグレジスタの設定
-  LDA #(CRTC2::WF|1)              ; f1書き込み
-  STA CRTC2::CONF
-  LDA #(CRTC2::TT|0)              ; 16色モード
-  STA CRTC2::CONF
-  ; 塗りつぶし
-  LDA #$FF
-  JSR FILL                        ; 塗りつぶし
-  ; 表示フレームセット
-  LDA #%01010101                  ; f1表示
-  STA CRTC2::DISP
-.endmac
+.CODE
 
 ; -------------------------------------------------------------------
 ;                          カーソルティック
 ; -------------------------------------------------------------------
 .macro tick_cursor
   ; プレイヤ移動
+  erase_cursor
   ; X
   LDA ZP_CURSOR_X
   CLC
@@ -80,6 +70,29 @@ RL_MARGIN = 4         ; 左右のマージン
   BCS @SKP_NEW_Y
   STA ZP_CURSOR_Y
 @SKP_NEW_Y:
+  draw_cursor
+.endmac
+
+.macro erase_cursor
+  LDA #(CRTC2::WF|2)          ; f2書き込み
+  STA CRTC2::CONF
+  LDA ZP_CURSOR_X
+  STA CRTC2::PTRX
+  LDA ZP_CURSOR_Y
+  STA CRTC2::PTRY
+  LDA #COL_CORSOR_BG
+  STA CRTC2::WDAT
+.endmac
+
+.macro draw_cursor
+  LDA #(CRTC2::WF|2)          ; f2書き込み
+  STA CRTC2::CONF
+  LDA ZP_CURSOR_X
+  STA CRTC2::PTRX
+  LDA ZP_CURSOR_Y
+  STA CRTC2::PTRY
+  LDA #COL_CORSOR_FG
+  STA CRTC2::WDAT
 .endmac
 
 ; -------------------------------------------------------------------
@@ -87,6 +100,7 @@ RL_MARGIN = 4         ; 左右のマージン
 ; -------------------------------------------------------------------
 .macro tick_pad
 TICK_PAD:
+  mem2mem16 ZP_PADSTAT_PREV,ZP_PADSTAT
   JSR PAD_READ                ; パッド状態更新
   STZ ZP_CURSOR_DY
   STZ ZP_CURSOR_DX
@@ -114,13 +128,88 @@ TICK_PAD:
   BBS7 ZP_PADSTAT,@SKP_B      ; B button
   JSR PUT_DOT
 @SKP_B:
+  BBS6 ZP_PADSTAT,@SKP_Y      ; Y button
+  BBR6 ZP_PADSTAT_PREV,@SKP_Y ;  押下のみ
+  JSR TOGGLE_FRAME
+@SKP_Y:
 .endmac
 
-.CODE
+START:
+  JMP INIT
+
+TOGGLE_FRAME:
+  LDA ZP_DISP_FRAME
+  CMP #%01010101
+  BEQ @SET_2
+@SET_1:
+  LDA #%01010101
+  BRA @SET_REG
+@SET_2:
+  LDA #%10011001
+@SET_REG:
+  STA ZP_DISP_FRAME
+  STA CRTC2::DISP
+  RTS
+
+; 画面全体をAの値で埋め尽くす
+FILL:
+  STZ CRTC2::PTRX
+  STZ CRTC2::PTRY
+  STA CRTC2::WDAT
+  LDY #$C0
+FILL_LOOP_V:
+  LDX #$80
+FILL_LOOP_H:
+  LDA CRTC2::REPT
+  DEX
+  BNE FILL_LOOP_H
+  DEY
+  BNE FILL_LOOP_V
+  RTS
+
+PUT_DOT:
+  ; カーソル位置に点を打つ
+  LDA #(CRTC2::WF|1)          ; f1書き込み
+  STA CRTC2::CONF
+  LDA ZP_CURSOR_X
+  STA CRTC2::PTRX
+  LDA ZP_CURSOR_Y
+  STA CRTC2::PTRY
+  LDA #$88
+  STA CRTC2::WDAT
+  RTS
+
+INIT_CRTC:
+  ; CRTCを初期化
+  LDA #%10000000                  ; ChrBox off
+  STA CRTC2::CHRW
+
+  ; コンフィグレジスタの設定 f1
+  LDA #(CRTC2::WF|1)              ; f1書き込み
+  STA CRTC2::CONF
+  LDA #(CRTC2::TT|0)              ; 16色モード
+  STA CRTC2::CONF
+  LDA #$FF                        ; 塗りつぶし
+  JSR FILL
+
+  ; コンフィグレジスタの設定 f2
+  LDA #(CRTC2::WF|2)              ; f2書き込み
+  STA CRTC2::CONF
+  LDA #(CRTC2::TT|0)              ; 16色モード
+  STA CRTC2::CONF
+  LDA #$FF                        ; 塗りつぶし
+  JSR FILL
+
+  ; 表示フレームセット
+  JSR TOGGLE_FRAME
+  RTS
+
 INIT:
   ; ---------------------------------------------------------------
   ;   初期化
-  init_crtc
+  LDA #%01010101
+  STA ZP_DISP_FRAME
+  JSR INIT_CRTC
   ; ---------------------------------------------------------------
   ;   IOレジスタの設定
   ; ---------------------------------------------------------------
@@ -172,32 +261,7 @@ VBLANK:
   ;   ティック終端
   JMP (ZP_VB_STUB)            ; 片付けはBCOSにやらせる
 
-PUT_DOT:
-  ; カーソル位置に点を打つ
-  LDA ZP_CURSOR_X
-  STA CRTC2::PTRX
-  LDA ZP_CURSOR_Y
-  STA CRTC2::PTRY
-  LDA #$88
-  STA CRTC2::WDAT
-  RTS
-
-; 画面全体をAの値で埋め尽くす
-FILL:
-  STZ CRTC2::PTRX
-  STZ CRTC2::PTRY
-  STA CRTC2::WDAT
-  LDY #$C0
-FILL_LOOP_V:
-  LDX #$80
-FILL_LOOP_H:
-  LDA CRTC2::REPT
-  DEX
-  BNE FILL_LOOP_H
-  DEY
-  BNE FILL_LOOP_V
-  RTS
-
+; PAD読み取りルーチン
 PAD_READ:
   LDA #BCOS::BHA_CON_RAWIN_NoWaitNoEcho  ; キー入力チェック
   syscall CON_RAWIN
